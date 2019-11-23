0662fdaff9e0b4da12309334a8fe55af746804e5
admin <zacharyjsanders@gmail.com>
2019-11-23 09:58:28 -0700

    testing cron via email

--- plugin.php
+++ plugin.php
@@ -15,71 +15,59 @@
  	exit;
  }
 
-
+////////////////////////////////////////////////////////////////////////////
+// Setup Cron
 ////////////////////////////////////////////////////////////////////////////
 
-// add our non-default cron schedule by tapping into ‘cron_schedules’ filter
- add_filter( 'cron_schedules', 'bl_add_cron_intervals' );
-
- // setup custom interval
- function bl_add_cron_intervals( $schedules ) {
-
-    $schedules['60seconds'] = array( // Provide the programmatic name to be used in code
-       'interval' => 60, // Intervals are listed in seconds
-       'display'  => __('Every 60 Seconds') // Easy to read display name (WP-Control plugin for ex. )
-    );
-    return $schedules; // Do not forget to give back the list of schedules
-
- }
-
-///////////////////////////////////////////////////////////////////////////
-
- // schedule our custom hook to run every 60s if it hasn't already been scheduled
-
- if( !wp_next_scheduled( 'bl_cron_hook' ) ) {
-    wp_schedule_event( time(), '60seconds', 'bl_cron_hook');
-
- }
- // create our own custom hook. first parameter is the name of the hook, and the second is the name of our function to call
- add_action( 'bl_cron_hook', 'pull_reviews' );
-
- // $args = array(false);
- // function schedule_my_cron() {
- //
- //     wp_schedule_event(time(), '60seconds', 'bl_cron_hook', $args);
- // }
- //
- //
- // if(!wp_next_scheduled('bl_cron_hook', $args )) {
- //
- //     add_action('init', 'pull_reviews');
- //
- // }
-
-///////////////////////////////////////////////////////////////////////////////
- function pull_reviews() {
-  echo 'it works';
-
- }
-
-
-/////////////////////////////////////////////////////////////////////////////
-
- // deactivation of cron job
- register_deactivation_hook( __FILE__, 'bl_deactivate' );
-
- function bl_deactivate() {
-    $timestamp = wp_next_scheduled( 'bl_cron_hook' );
-    wp_unschedule_event($timestamp, 'bl_cron_hook' );
- }
-
+add_action('wp', 'myplugin_schedule_cron');
+function myplugin_schedule_cron() {
+  if ( !wp_next_scheduled( 'myplugin_cron' ) )
+    wp_schedule_event(time(), 'customTime', 'myplugin_cron');
+}
 
 ////////////////////////////////////////////////////////////////////////////
-// add_action('wp_enqueue_scripts','pull_reviews');
-//
-// function pull_reviews() {
-//     wp_enqueue_script( 'init.php', plugins_url( './init.php', __FILE__ ));
-// }
+// Cron Function
+////////////////////////////////////////////////////////////////////////////
+
+// the CRON hook for firing function
+add_action('myplugin_cron', 'myplugin_cron_function');
+// add_action('wp_head', 'myplugin_cron_function'); //test on page load
+
+// the actual function
+function myplugin_cron_function() {
+    // see if fires via email notification
+    // wp_mail('zacharyjsanders@gmail.com','Cron Worked', date('r'));
+    $url = 'https://jsonplaceholder.typicode.com/users'; // testing url
+    $data = file_get_contents($url); // put the contents of the file into a variable
+
+
+    file_put_contents('test.json', json_encode($data)); // write json to test.js
+}
+
+////////////////////////////////////////////////////////////////////////////
+// CUSTOM TIME INTERVAL FOR CRON
+////////////////////////////////////////////////////////////////////////////
+
+add_filter('cron_schedules', 'myplugin_cron_add_intervals');
+function myplugin_cron_add_intervals( $schedules ) {
+  $schedules['customTime'] = array(
+    'interval' => 30,
+    'display' => __('Every 30sec')
+  );
+  return $schedules;
+}
+
+////////////////////////////////////////////////////////////////////////////
+// Deactivate Cron
+////////////////////////////////////////////////////////////////////////////
+
+ register_deactivation_hook( __FILE__, 'myplugin_cron_deactivate' );
+
+ function myplugin_cron_deactivate() {
+    $timestamp = wp_next_scheduled( 'myplugin_cron' );
+    wp_unschedule_event($timestamp, 'myplugin_cron' );
+ }
+
 
 
 
